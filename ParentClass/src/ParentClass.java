de5b2a98443b2352f8001977c491ca1ca25971dc
Tmkachhia <kachhiatarjani@gmail.com>
2020-01-24 13:17:05 +0000

    done

---
+++ ParentClass/src/ParentClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ParentClass {
+    public static void main(String [] args){
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("enter employee name :");
+    String empName = scanner.next();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+
+    }
+}
